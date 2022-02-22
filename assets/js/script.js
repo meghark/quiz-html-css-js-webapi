871963b6f344e96d503f997aa1ebeb0f727ba62c
Megha Raj <megha.nambiar@gmail.com>
2022-02-21 17:34:27 -0800

    Added a json variable with questions for the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const jsonData = {
+    "quiz":[
+        {"question": "Arrays in JavaScript can be used to store ________.",
+         "id" : "1",
+          "options":[{"id":"1","option":"numbers and strings"}, 
+                      {"id":"2", "option": "other arrays"},
+                      {"id": "3", "option": "boolens"},
+                      {"id": "4", "option" : "all of the above" }
+                    ],             
+           "answer": "4"         
+         },
+         {"question": "A very userful tool used during development and debugging for printing content to debugger is:",
+           "id" : "2",
+           "options":[{"id":"1","option":"JavaScript"}, 
+                      {"id":"2", "option": "terminal/bash"},
+                      {"id":"3", "option": "for loops"},
+                      {"id":"4", "option" : "console.log" }
+                    ],             
+             "answer": "4"         
+           },
+           {"question": "Which of the following is NOT a reason to validate a user's responses?",
+           "id" : "3",
+           "options":[{"id":"1","option":"Offers the user an opportunity to enter a correct response."}, 
+                      {"id":"2", "option": "Reduces bogus answers getting stored in the database."},
+                      {"id":"3", "option": "Improves the user experience."},
+                      {"id":"4", "option" : "Increases the overall quality of the user data." }
+                    ],             
+             "answer": "3"         
+           },
+           {"question": "Which statement correctly stores data into the Web Storage API?",
+           "id" : "4",
+           "options":[{"id":"1","option":"localStorage.getItem('lunch', 'sandwich');"}, 
+                      {"id":"2", "option": "localStorage.setItem('lunch', 'sandwich');"},
+                      {"id":"3", "option": "getItem.localStorage.('lunch', 'sandwich');"},
+                      {"id":"4", "option" : "setItem.localStorage('lunch', 'sandwich');" }
+                    ],             
+             "answer": "4"         
+           }  
+       ] 
+ };
+
+ 
